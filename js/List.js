51042d77f92be13367e56907295d61665e5b4d0f
junghoon <hun2_2@naver.com>
2022-03-22 19:41:44 +0900

    List update ing..

---
+++ js/List.js
@@ -0,0 +1,49 @@
+let folioList = {
+  contents: [
+    {
+      type: 'Publishing',
+      typeDetail: 'Build',
+      client: '네이버(주)',
+      date: '2021-11 ~ 2022-01',
+      title: 'NAVER VIBE 서비스 소개 페이지 리뉴얼 구축',
+      description: ''
+    },
+    {
+      type: 'Publishing',
+      typeDetail: 'Build, Manage',
+      client: '네이버클라우드 주식회사',
+      date: '2021-11 ~ 2022-01',
+      title: 'NCP NAVER Cloud Summit 2021 웹사이트 구축 및 운영',
+      description: ''
+    },
+    {
+      type: 'Publishing',
+      typeDetail: 'Build',
+      client: 'CJ',
+      date: '2021-09 ~ 2021-11',
+      title: 'M3(mnet smart+) 웹사이트 구축',
+      description: 'Home, Artist > Board, Contents, Vote 페이지'
+    },
+    {
+      type: 'Publishing',
+      typeDetail: 'Build, Manage',
+      client: '네이버클라우드 주식회사',
+      date: '2021-08 ~ 2021-10',
+      title: 'NCP GAME X CONFERENCE 웨비나 웹사이트 구축 및 운영',
+      description: ''
+    },
+    {
+      type: 'Publishing',
+      typeDetail: 'Build, Manage',
+      client: '아모레퍼시픽',
+      date: '2021-05 ~ 2021-07',
+      title: '그린사이클 캠페인 마이크로 사이트 구축',
+      description: ''
+    },
+  ]
+};
+
+let mainList = new Vue({
+  el: '__',
+  data: folioList,
+});
