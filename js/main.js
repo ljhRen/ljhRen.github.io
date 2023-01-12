7fff77490f5f30e9544429243a22a721b7f9ef2e
Hun2 <hun2_2@naver.com>
2023-01-12 13:58:23 +0900

    menu update, responsive develop ing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+const htmlDom = document.querySelector('html');
+const menuBtn = document.querySelector('.header-wrap #nav-wrap .menu-btn');
+const menuNavWrap = document.getElementById('nav-wrap');
+
+function menuHandler(e) {
+  e.preventDefault();
+  let flag = menuNavWrap.className;
+
+  if (flag === 'active') {
+    menuNavWrap.classList.remove('active');
+    htmlDom.classList.remove('fixed');
+    console.log('Open!');
+  } else {
+    menuNavWrap.classList.add('active');
+    htmlDom.classList.add('fixed');
+    console.log('Close!');
+  }
+}
+menuBtn.addEventListener('click', menuHandler);
+
 jQuery(document).ready(function() {
   // Start..
   
