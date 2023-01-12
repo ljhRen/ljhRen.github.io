f295acb843142fd509c446f6f391d4fd80ecd417
Hun2 <hun2_2@naver.com>
2023-01-12 15:06:02 +0900

    update ing..

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const htmlDom = document.querySelector('html');
 const menuBtn = document.querySelector('.header-wrap #nav-wrap .menu-btn');
 const menuNavWrap = document.getElementById('nav-wrap');
+let winWidth = window.innerWidth;
 
 function menuHandler(e) {
   e.preventDefault();
@@ -9,14 +10,25 @@ function menuHandler(e) {
   if (flag === 'active') {
     menuNavWrap.classList.remove('active');
     htmlDom.classList.remove('fixed');
-    console.log('Open!');
+    console.log('Close!');
   } else {
     menuNavWrap.classList.add('active');
     htmlDom.classList.add('fixed');
-    console.log('Close!');
+    console.log('Open!');
   }
 }
 menuBtn.addEventListener('click', menuHandler);
+
+window.addEventListener('resize', () => {
+  // window Width
+  winWidth = window.innerWidth;
+
+  if (winWidth > 769) {
+    menuNavWrap.classList.remove('active');
+    htmlDom.classList.remove('fixed');
+  }
+
+});
 
 jQuery(document).ready(function() {
   // Start..
